63c9503ff2db71044f8bf119c61e800273243e92
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-03 21:12:27 +0530

    ippudupakka

--- Policy/Initiative/main.tf
+++ Policy/Initiative/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   required_providers {
       azurerm = {
           source = "hashicorp/azurerm"
-          version = ">=1.42.0"
+          version = "3.54.0"
       }
     }
   }
