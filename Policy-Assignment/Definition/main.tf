ab79856654cea9e6a4b5ddf7a0f06a6715205398
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-08 16:04:41 +0530

    checknow

--- Policy-Assignment/Definition/main.tf
+++ Policy-Assignment/Definition/main.tf
@@ -11,11 +11,11 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_subscription_policy_assignment" "dds" {
-  name                 = "Deploy-Diagnostic-Setting"
+resource "azurerm_subscription_policy_assignment" "subnsg" {
+  name                 = "Subnet-Should-have-NSG"
   subscription_id      = var.cust_scope
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/2465583e-4e78-4c15-b6be-a36cbc7c8b0f"
-  description          = "Deploys the diagnostic settings for Azure Activity to stream subscriptions audit logs to a Log Analytics workspace to monitor subscription-level events"
-  display_name         = "Configure Azure Activity logs to stream to specified Log Analytics workspace"
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e71308d3-144b-4262-b144-efdc3cc90517"
+  description          = "Protect your subnet from potential threats by restricting access to it with a Network Security Group (NSG). NSGs contain a list of Access Control List (ACL) rules that allow or deny network traffic to your subnet."
+  display_name         = "Subnets should be associated with a Network Security Group"
 }
 
