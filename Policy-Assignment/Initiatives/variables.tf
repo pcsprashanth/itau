3527c232895703977e776a65aaa91190610c1ff4
pcsprashanth <52338785+pcsprashanth@users.noreply.github.com>
2023-05-19 14:54:24 +0530

    Update variables.tf

--- Policy-Assignment/Initiatives/variables.tf
+++ Policy-Assignment/Initiatives/variables.tf
@@ -1,3 +1,3 @@
 variable "cust_scope" {
-  default = "/subscriptions/25229114-2ec3-4b44-bb5b-649a554894bc"
+  default = "/subscriptions/25229114-2ec3-4b44-bb5b-649a554894bc/managementgroup/mg-itaudev-sandbox"
 }
